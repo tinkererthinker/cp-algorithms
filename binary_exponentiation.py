fdf02ddede5901071460587539eac8789415d893
Trazy <trang@trangla.de>
2025-01-13 13:05:35 +0100

    modified bin exp function to include modular arithmetic

--- binary_exponentiation.py
+++ binary_exponentiation.py
@@ -12,3 +12,15 @@ def binary_exponentiation(base, exponent):
 
 # Example usage
 print(binary_exponentiation(3, 13))  # Output: 1594323
+
+def mod_binary_exponentiation(base, exponent, mod):
+    result = 1
+    base %= mod
+    while exponent > 0:
+        if exponent & 1:
+            result = (result * base) % mod
+        base = (base * base) % mod
+        exponent >>= 1
+    return result
+
+print(mod_binary_exponentiation(3, 1000000, 1000000007))  # Output: 624098969
